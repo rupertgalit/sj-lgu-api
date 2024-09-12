974e2544e69dc28110fee14d071236dd89e91bf9
Paul Cullen <paulcullen.developer@gmail.com>
2024-09-12 14:54:39 +0800

    update cors issue

---
+++ database/migrations/2024_09_11_074143_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('Username')->nullable();
+            $table->string('Password')->nullable();
+            $table->boolean('Is_active')->nullable();
+            $table->string('Status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
